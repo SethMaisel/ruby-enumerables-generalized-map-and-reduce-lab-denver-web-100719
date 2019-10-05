c2ab96b5126a09ff1b5bc52cfc7d31906abedd4f
Seth Maisel <seth.maisel@gmail.com>
2019-10-05 02:50:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,8 @@
 def my_own_map(array)
   new_array = []
-  (map([array]){|n| n * -1})
   i = 0
   while i < array.length
+  map([array]){|n| n * -1})
   new_array.push(yield(array[i]))
   i + 1
   end
